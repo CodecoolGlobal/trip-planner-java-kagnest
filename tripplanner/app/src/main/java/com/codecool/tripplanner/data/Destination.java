61eb90579589cb06221bc49c584ca032c741fc73
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-05-28 13:34:36 +0200

    Create constructor and getter methods

--- tripplanner/app/src/main/java/com/codecool/tripplanner/data/Destination.java
+++ tripplanner/app/src/main/java/com/codecool/tripplanner/data/Destination.java
@@ -10,7 +10,6 @@ import androidx.room.PrimaryKey;
 public class Destination {
 
     @PrimaryKey(autoGenerate = true)
-    @NonNull
     private String id;
 
     @NonNull
@@ -20,5 +19,31 @@ public class Destination {
     @NonNull
     private String url;
 
+    @NonNull
     private String coordinates;
+
+    public Destination(@NonNull String destinationName, @NonNull String url, @NonNull String coordinates) {
+        this.destinationName = destinationName;
+        this.url = url;
+        this.coordinates = coordinates;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    @NonNull
+    public String getUrl() {
+        return url;
+    }
+
+    @NonNull
+    public String getCoordinates() {
+        return coordinates;
+    }
 }
