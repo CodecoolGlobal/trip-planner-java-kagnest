abd5d7631e43980b307be9f2d63d163f3b1b6762
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-05-29 22:31:42 +0200

    Add new activity AddDestinationActivity

---
+++ tripplanner/app/src/main/java/com/codecool/tripplanner/destinationedit/AddDestinationActivity.java
@@ -0,0 +1,16 @@
+package com.codecool.tripplanner.destinationedit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codecool.tripplanner.R;
+
+public class AddDestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_destination);
+    }
+}
